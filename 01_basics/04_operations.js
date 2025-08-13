63244c34972163e9a723b9500f368a7062689d67
zeeshan77777 <zeeshanumar.revnix@gmail.com>
2025-08-13 07:00:54 +0000

    modify data

--- 01_basics/04_operations.js
+++ 01_basics/04_operations.js
@@ -8,10 +8,10 @@ let str3 = str1 + str2
 // console.log(str3)
 
 // console.log(3 + 1);
-// console.log("3" + 1);
-// console.log(3 + "1");
+console.log("3" + 1);
+// console.log(2 - "2");
 // console.log("3" + "1");
-// console.log(3 + 1 + "4");
+// console.log(3 + "2" + 3);
 
 // console.log(true)
 // console.log(+true) //this is another way of converting boolean into number
@@ -28,5 +28,5 @@ const b = ++a;
 
 console.log(`a:${a}, b:${b}`);
 // Expected output: "a:4, b:4"
-let x = 3;
-console.log(x++);
+// let x = 3;
+// console.log(x++);
--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -7,7 +7,7 @@ const gameName = new String('Call Of Duty') //we declare a string to a variable 
 
 // console.log(gameName.length); //it tells the lenght of the string
 
-console.log(gameName.__proto__); //it tells the 
+console.log(gameName.__proto__); //it tells the all the prototypes that we can use with this string
 
 // console.log(gameName[5]); 
 console.log(gameName.toUpperCase());
@@ -16,5 +16,12 @@ console.log(gameName.indexOf('D'));
 
 const newString = gameName.substring(0 , 5)
 console.log(newString);
+
+const anotherString = gameName.slice(0 , 8)
+console.log(anotherString);
+
+const anotherNewString = "       Zee       " 
+console.log(anotherNewString);
+console.log(anotherNewString.trim());
 
 
