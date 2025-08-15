48c741752795183d7e3cf57600b0ce7001ddbc2c
zeeshan77777 <zeeshanumar.revnix@gmail.com>
2025-08-15 13:48:31 +0000

    new file

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+
+if(true){
+    let a = 24
+    const b = 33
+    var c = 69
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
+//Anythin that is written inside {} is a block scope element and everything outside is global scope element.
+
+/* Note:  anything that is written in a global can be used in a block scope but anything that is written in 
+a block scope can not be used in a global scope 
+*/
